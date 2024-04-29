51dd1143fab881c3f468d6b98b5fb04ca9ac7556
bmancilla23 <bmancilla119@gmail.com>
2024-04-29 14:54:49 -0500

    Creación de layout child

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,20 @@
+
+// shortcut lmrc create a layout
+
+import { Navbar } from "@/components";
+
+export default function PublicLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+    <Navbar/>
+    <main className='flex flex-col items-center p-24'>
+      <span className='text-lg'>Hola Mundo</span>
+      {children}
+    </main>
+    </>
+  );
+}
