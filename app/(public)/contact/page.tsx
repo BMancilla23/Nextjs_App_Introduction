6f832a9994b5ad29961ca48675e3339e986f4d40
bmancilla23 <bmancilla119@gmail.com>
2024-04-29 12:58:24 -0500

    Página contact creada

---
+++ app/(public)/contact/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const metadata = {
+  title: 'Contact Page',
+  description: 'Esta es la página de contacto de la empresa',
+};
+
+type Props = {}
+
+const ContactPage = (props: Props) => {
+  return (
+    <div className='text-7xl'>ContactPage</div>
+  )
+}
+
+export default ContactPage
